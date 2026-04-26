7515fca179ae5a6c907e5f69b3f5b7cf1fac02a9
harrihonkala <harrip.honkala@gmail.com>
2026-04-26 09:24:06 +0300

    Fix PWA paths for iOS home screen

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/treenipaiva/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
