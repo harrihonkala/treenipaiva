91aecb226804a523b30fcafe0d94ff014b54fbcc
harrihonkala <harrip.honkala@gmail.com>
2026-08-10 15:07:59 +0300

    Initialize Supabase data sync

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import AuthGate from './AuthGate.jsx'
+import { initSupabaseSync } from './lib/supabaseSync'
+
+initSupabaseSync()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
